bddd051c1e8d539b47b5fc4e62b5e1352b84f5ab
Magdalena.V <magdalena.vurmovska@hotmail.com>
2025-12-16 12:51:39 +0100

    Add test setup file for Vitest

---
+++ strajk-bowling/src/test/setup.js
@@ -0,0 +1,13 @@
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { server } from "../__tests__/mocks/server";
+
+beforeAll(() => server.listen({onUnhandledRequest: 'error'}))
+
+afterEach(() => {
+    cleanup()
+server.resetHandlers()
+})
+
+afterAll(() => server.close())
